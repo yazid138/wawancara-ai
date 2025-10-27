518fa7b7cd6540b421f5415f3d8a30ae2a06da2d
yazid138 <ma39isy@gmail.com>
2025-10-27 18:39:19 +0700

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import sendResponse from "@/utils/responseHandler";
 import { validate } from "@/utils/validation";
 import { Router, Request, Response } from "express";
+
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    validate(res, {name: 'string'}, req.query);
-    sendResponse(res, { status: 200, message: "Welcome to the API" });
+	validate(res, { name: "string" }, req.query);
+	sendResponse(res, { status: 200, message: "Welcome to the API" });
 });
 
 export default router;
