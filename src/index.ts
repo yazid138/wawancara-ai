fcb493495ad1c4a975504e29572d3b64815729af
yazid138 <ma39isy@gmail.com>
2025-10-27 20:31:39 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import express, { NextFunction, Request, Response } from "express";
 import logger from "morgan";
 import config from "@/config";
 import NotFoundException from "@/exception/NotFoundException";
-import HttpException from "@/types/exception";
 import routes from "@/routes/";
-import sendResponse from "./utils/responseHandler";
 import passport from "passport";
 import dotenv from "dotenv";
+import errorHandler from "@/middleware/errorHandler";
 
 dotenv.config();
 
@@ -22,14 +21,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 	throw new NotFoundException();
 });
 
-app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
-	try {
-		sendResponse(res, { status: err.status, message: err.message, error: err.error || undefined });
-	} catch (error) {
-		console.error(error);
-		sendResponse(res, { status: 500, message: "Internal Server Error" });
-	}
-});
+app.use(errorHandler());
 
 app.listen(config.port, () => {
 	console.log(`Server is running on ${config.baseUrl}:${config.port}`);
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import HttpException from "@/types/exception";
+import sendResponse from "@/utils/responseHandler";
+
+export default () => (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+	try {
+		sendResponse(res, { status: err.status, message: err.message, error: err.error || undefined });
+	} catch (error) {
+		console.error(error);
+		sendResponse(res, { status: 500, message: "Internal Server Error" });
+	}
+};
