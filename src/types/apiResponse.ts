a67ebbd26a166813bc0574d10c062b59e43b54a3
yazid138 <ma39isy@gmail.com>
2025-10-28 14:25:42 +0700

    commit

--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -1,6 +1,11 @@
 export default interface ApiResponse<T> {
   status: number;
   message: string;
+  metadata?: {
+    total: number;
+    page: number;
+    limit: number;
+  };
   data?: T;
   error?: any;
 }
