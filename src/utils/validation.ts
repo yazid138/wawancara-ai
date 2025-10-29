a6ca75ab85d0a04702a6d7ce5f9e8108f711f365
yazid138 <ma39isy@gmail.com>
2025-10-29 08:51:36 +0700

    commit

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,13 +1,19 @@
-import Validator, { ValidationError, ValidationSchema } from "fastest-validator";
+import Validator, {
+  ValidationError,
+  ValidationSchema,
+} from "fastest-validator";
 import BadRequestException from "@/exception/BadRequestException";
 
 const v = new Validator();
 
 export default <T>(schema: ValidationSchema<T>, data: T) => {
-	const check = v.compile(schema);
-	const result = check(data || {});
-	if (result !== true) {
-		throw new BadRequestException("Validation Error", result as ValidationError[]);
-	}
-	return true;
+  const check = v.compile(schema);
+  const result = check(data || {});
+  if (result !== true) {
+    throw new BadRequestException(
+      "Validation Error",
+      result as ValidationError[],
+    );
+  }
+  return true;
 };
