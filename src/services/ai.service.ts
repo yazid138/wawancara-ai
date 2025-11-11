780bddafacd7d4fdfed30ace6c80793fb4f1c0b4
yazid138 <ma39isy@gmail.com>
2025-11-11 16:06:39 +0700

    commit

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -5,21 +5,10 @@ const client = new OpenAI({
   apiKey: config.openAIKey,
 });
 
-export const validateInterviewInput = async (pertanyaan: string, message: string) => {
+export const validateInterviewInput = async (pertanyaan: string, jawaban: string) => {
   const { output_text } = await client.responses.create({
     model: config.openAIModel,
-    input: [
-      { role: "system", content: "kamu adalah seorang hrd yang akan mewawancarai mahasiswa untuk bekerja di perusahaan" },
-      { role: "user", content: `apakah input berikut sudah sesuai dengan ketentuan wawancara?:
-        [
-      { role: "assistant", content: ${pertanyaan} },
-      { role: "user", content: '${message}' },
-    ]
-      kembalikan dalam format {
-        valid: true jika sesuai ketentuan wawancara, false jika tidak sesuai ketentuan wawancara,
-        keterangan: penjelasan singkat mengapa input tersebut sesuai atau tidak sesuai dengan ketentuan wawancara
-      }` },
-    ],
+    input: `Tolong periksa apakah jawaban berikut sesuai dengan pertanyaan yang diajukan.\n\nPertanyaan: ${pertanyaan}\n\nJawaban: ${jawaban}\n\nKembalinkan jawaban dengan format {"valid": true atau false, "alasan": "alasan jika tidak valid"}.`,
   });
   return output_text;
 }
