1a455658d903137d436898e395209f850ea5ff80
yazid138 <ma39isy@gmail.com>
2025-11-26 15:31:28 +0700

    commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
+import { PRODUCTION } from "@/utils/constants";
 
 export default {
-  env: process.env.ENV || "production",
+  env: process.env.ENV || PRODUCTION,
   baseUrl: process.env.BASE_URL || "http://localhost",
   port: process.env.PORT || 3000,
   secretKey: process.env.SECRET_KEY || "secretkey",
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,19 @@
 import winston from "winston";
 import config from "@/config";
+import { PRODUCTION } from "@/utils/constants";
 
 const { combine, timestamp, printf } = winston.format;
 
 const logger = winston.createLogger({
-  level: "info",
+  level: config.env === PRODUCTION ? "info" : "debug",
   format: combine(
     timestamp(),
     printf(({ timestamp, level, message, ...meta }) => {
       return `${timestamp} [${level.toUpperCase()}] ${message} ${Object.keys(meta).length ? JSON.stringify(meta) : ""}`;
     }),
   ),
-  handleExceptions: config.env === "development",
-  handleRejections: config.env === "development",
+  handleExceptions: config.env !== PRODUCTION,
+  handleRejections: config.env !== PRODUCTION,
   transports: [new winston.transports.File({ filename: "application.log" })],
 });
 
