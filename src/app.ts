748ca07b6d6f190273c5e1190561debad9ed087a
yazid138 <ma39isy@gmail.com>
2026-05-05 15:47:53 +0700

    feat: add CORS support and update dependencies
    - Added CORS middleware to the Express app for handling cross-origin requests.
    - Updated the 'cors' package to version 2.8.6.
    - Added type definitions for 'cors' to devDependencies in package.json.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
 import routes from "@/routes/";
 import passport from "passport";
 import error from "@/middleware/error";
@@ -7,6 +8,7 @@ import "@/config/passport";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
