c55047026d9aedbe32bbd93238046827142e4341
yazid138 <ma39isy@gmail.com>
2026-05-20 10:18:34 +0700

    feat: enhance question categories and add new soft skill questions for adaptability, communication, leadership, and problem solving

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -53,6 +53,10 @@ const main = async () => {
     "Backend",
     "Database",
     "Security",
+    "Communication",
+    "Problem Solving",
+    "Leadership",
+    "Self Confidence",
   ];
 
   await prisma.questionCategory.createMany({
@@ -131,8 +135,13 @@ const main = async () => {
 
     // SOFTSKILL CATEGORY (IMPORTANT)
     if (q.type === QuestionType.SOFTSKILL) {
-      const answerCategories: { label: string; score: number }[] =
-        await aiService.generateAnswerCategories(q.content);
+      let answerCategories: { label: string; score: number }[] = [];
+      if (q.categoryAnswer) {
+        answerCategories = q.categoryAnswer;
+      } else {
+        answerCategories =
+          await aiService.generateAnswerCategories(q.content);
+      }
       await prisma.answerCategory.createMany({
         data: answerCategories.map((cat) => ({
           questionId: question.id,
@@ -175,40 +184,253 @@ const main = async () => {
 
     // SOFTSKILL
     {
-      content: "Ceritakan pengalaman kerja tim.",
+      content: "Bagaimana anda menyesuaikan diri dengan aturan yang berlaku di tempat magang?",
       type: QuestionType.SOFTSKILL,
       difficulty: "MEDIUM",
-      category: "Teamwork",
+      category: "Adaptability", 
+      categoryAnswer: [
+        { label: "Mudah Beradaptasi", score: 5 }, 
+        { label: "Bisa Beradaptasi", score: 3 }, 
+        { label: "Sulit Beradaptasi", score: 2 }, 
+        { label: "Tidak Mau Beradaptasi", score: 1 }
+      ],
     },
     {
-      content: "Bagaimana menangani konflik?",
+      content: "Apakah anda membutuhkan waktu untuk penyesuaian dengan rekan kerja?",
       type: QuestionType.SOFTSKILL,
       difficulty: "MEDIUM",
-      category: "Teamwork",
+      category: "Adaptability", 
+      categoryAnswer: [
+        { label: "Saya tidak membutuhkan waktu untuk penyesuaian dengan rekan kerja", score: 5 },
+        { label: "Saya membutuhkan waktu yang singkat untuk penyesuaian dengan rekan kerja", score: 3 },
+        { label: "Saya membutuhkan waktu yang cukup lama untuk penyesuaian dengan rekan kerja", score: 2 },
+        { label: "Saya tidak bisa beradaptasi dengan rekan kerja", score: 1 }
+      ],
     },
     {
-      content: "Bagaimana mengatur waktu?",
+      content: "Bagaimana Anda menghadapi perubahan yang terjadi pada situasi kerja?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Adaptability", 
+      categoryAnswer: [
+        { label: "Saya dapat menyesuaikan diri, percaya diri dengan perubahan", score: 5 },
+        { label: "Saya biasa/cuek menyesuaikan diri dengan perubahan", score: 3 },
+        { label: "Saya takut/grogi menyesuaikan diri dengan perubahan", score: 2 },
+        { label: "Saya tidak bisa menyesuaikan diri dengan perubahan", score: 1 }
+      ],
+    },
+    {
+      content: "Bagaimana jika perusahaan membutuhkan skill yang diluar kemampuanmu, apakah kamu siap untuk belajar?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Adaptability", 
+      categoryAnswer: [
+        { label: "Saya sangat siap untuk belajar dan mengembangkan skill baru", score: 5 },
+        { label: "Saya siap untuk belajar dan mengembangkan skill baru", score: 3 },
+        { label: "Saya kurang siap untuk belajar dan mengembangkan skill baru", score: 2 },
+        { label: "Saya tidak siap untuk belajar dan mengembangkan skill baru", score: 1 }
+      ],
+    },
+    {
+      content: "Bagaimana cara anda menyampaikan pendapat dalam tim?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Communication",
+      categoryAnswer: [
+        { label: "Saya sangat percaya diri dalam menyampaikan pendapat dalam tim", score: 5 },
+        { label: "Saya percaya diri dalam menyampaikan pendapat dalam tim", score: 4 },
+        { label: "Saya kurang percaya diri dalam menyampaikan pendapat dalam tim", score: 3 },
+        { label: "Saya tidak percaya diri dalam menyampaikan pendapat dalam tim", score: 2 },
+        { label: "Saya hanya diam tidak mau menyampaikan pendapat dalam tim", score: 1 }
+      ],
+    },
+    {
+      content: "Bagaimana cara anda menyampaikan pendapat kepada supervisor anda?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Communication",
+      categoryAnswer: [
+        { label: "Saya sangat percaya diri dalam menyampaikan pendapat kepada supervisor saya", score: 5 },
+        { label: "Saya percaya diri dalam menyampaikan pendapat kepada supervisor saya", score: 4 },
+        { label: "Saya kurang percaya diri dalam menyampaikan pendapat kepada supervisor saya", score: 3 },
+        { label: "Saya tidak percaya diri dalam menyampaikan pendapat kepada supervisor saya", score: 2 },
+        { label: "Saya hanya diam tidak mau menyampaikan pendapat kepada supervisor saya", score: 1 }
+      ],
+    },
+    {
+      content: "bagaimana cara anda menyampaikan pendapat di depan umum?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Communication",
+      categoryAnswer: [
+        { label: "Saya sangat percaya diri dalam menyampaikan pendapat di depan umum", score: 5 },
+        { label: "Saya percaya diri dalam menyampaikan pendapat di depan umum", score: 4 },
+        { label: "Saya kurang percaya diri dalam menyampaikan pendapat di depan umum", score: 3 },
+        { label: "Saya tidak percaya diri dalam menyampaikan pendapat di depan umum", score: 2 },
+        { label: "Saya hanya diam tidak mau menyampaikan pendapat di depan umum", score: 1 }
+      ],
+    },
+    {
+      content: "Seberapa yakin anda dapat menyelesaikan masalah yang menimpa anda?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Problem Solving",
+      categoryAnswer: [
+        { label: "Saya sangat yakin dapat menyelesaikan masalah yang menimpa saya", score: 5 },
+        { label: "Saya yakin dapat menyelesaikan masalah yang menimpa saya", score: 4 },
+        { label: "Saya kurang yakin dapat menyelesaikan masalah yang menimpa saya", score: 3 },
+        { label: "Saya tidak yakin dapat menyelesaikan masalah yang menimpa saya", score: 2 },
+        { label: "Saya tidak dapat menyelesaikan masalah yang menimpa saya", score: 1 }
+      ]
+    },
+    {
+      content: "Apa langkah pertama jika anda dihadapkan dengan maslah baru yang belum pernah anda hadapi sebelumnya?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Problem Solving",
+      categoryAnswer: [
+        { label: "Saya langsung mencari solusi", score: 5 },
+        { label: "Saya membutuhkan waktu untuk berpikir", score: 3 },
+        { label: "Saya langsung mencari pertolongan orang lain", score: 2 },
+        { label: "Saya langsung menyerah/tidak melakukan apapun", score: 1 }
+      ]
+    },
+    {
+      content: "Situai tersulit apa yang pernah anda alami, bagaimana anda mengatasinya?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Problem Solving",
+      categoryAnswer: [
+        { label: "Saya mengatasi masalah sendiri", score: 5 },
+        { label: "Saya mencoba mengatasi masalah lalu meminta bantuan orang lain", score: 3 },
+        { label: "Saya butuh bantuan orang lain untuk mengatasi masalah", score: 2 },
+        { label: "Saya tidak melakukan apapun untuk mengatasi masalah", score: 1 }
+      ]
+    },
+    {
+      content: "Apakah anda yakin bahwa anda memiliki jiwa kepemimpinan yang kuat? Ceritakan pengalaman anda secara singkat dalam memimpin tim!",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Leadership",
+      categoryAnswer: [
+        { label: "Saya sangat yakin memiliki jiwa kepemimpinan yang kuat", score: 5 },
+        { label: "Saya yakin memiliki jiwa kepemimpinan yang kuat", score: 4 },
+        { label: "Saya kurang yakin memiliki jiwa kepemimpinan yang kuat", score: 3 },
+        { label: "Saya tidak yakin memiliki jiwa kepemimpinan yang kuat", score: 2 },
+        { label: "Saya tidak memiliki jiwa kepemimpinan yang kuat", score: 1 }
+      ]
+    },
+    {
+      content: "Bagaimana Anda membangkitkan semangat tim ketika menghadapi hambatan?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Leadership",
+      categoryAnswer: [
+        { label: "Saya memberikan alternatif solusi dan memotivasi tim", score: 5 },
+        { label: "Saya memberikan solusi namun sisanya terserah pada tim", score: 4 },
+        { label: "Saya hanya memotivasi tim dan mengajak tim untuk mencari solusi bersama", score: 3 },
+        { label: "Saya membagi beban tugas dengan tim", score: 2 },
+        { label: "Saya tidak melakukan apa-apa untuk membangkitkan semangat tim", score: 1 }
+      ]
+    },
+    {
+      content: "Bagaimana sikap anda terkait saingan dan kompetitor anda?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Leadership",
+      categoryAnswer: [
+        { label: "Saya melihat saingan sebagai motivasi untuk terus berkembang", score: 5 },
+        { label: "Saya tidak terlalu memikirkan kompetitor", score: 3 },
+        { label: "Saya merasa tersaingi ketika menghadapi kompetitor", score: 2 },
+        { label: "Saya berusaha menjatuhkan kompetitor", score: 1 },
+      ]
+    },
+    {
+      content: "Seberapa sering anda memimpin sebuah tim atau kelompok?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Leadership",
+      categoryAnswer: [
+        { label: "Saya sangat sering memimpin sebuah tim atau kelompok", score: 5 },
+        { label: "Saya sering memimpin sebuah tim atau kelompok", score: 4 },
+        { label: "Saya kadang-kadang memimpin sebuah tim atau kelompok", score: 3 },
+        { label: "Saya jarang memimpin sebuah tim atau kelompok", score: 2 },
+        { label: "Saya tidak pernah memimpin sebuah tim atau kelompok", score: 1 },
+      ]
+    },
+    {
+      content: "Bagaimana anda mengatur jadwal keseharian anda?",
       type: QuestionType.SOFTSKILL,
       difficulty: "MEDIUM",
       category: "Time Management",
+      categoryAnswer: [
+        { label: "Saya terbiasa mengatur jadwal keseharian saya", score: 5 },
+        { label: "Saya hanya mengatur jadwal yang penting saja", score: 3 },
+        { label: "Saya jarang mengatur jadwal keseharian saya", score: 2 },
+        { label: "Saya tidak pernah mengatur jadwal keseharian saya", score: 1 },
+      ]
     },
     {
-      content: "Ceritakan saat harus adaptasi cepat.",
+      content: "Jika anda memiliki tugas yang sudah tenggat waktu, Apa yang anda lakukan?",
       type: QuestionType.SOFTSKILL,
       difficulty: "MEDIUM",
-      category: "Adaptability",
+      category: "Time Management",
+      categoryAnswer: [
+        { label: "Saya berusaha menyelesaikan tugas tersebut tepat waktu", score: 5 },
+        { label: "Saya mencoba menyelesaikan tugas tersebut lalu dikumpulkan seadanya", score: 3 },
+        { label: "Saya terlambat dalam menyelesaikan tugas", score: 2 },
+        { label: "Saya menyerah untuk menyelesaikan tugas tersebut", score: 1 },
+      ]
     },
     {
-      content: "Bagaimana menghadapi tekanan?",
+      content: "Apakah anda suka menunda pekerjaan?",
       type: QuestionType.SOFTSKILL,
       difficulty: "MEDIUM",
-      category: "Stress Management",
+      category: "Time Management",
+      categoryAnswer: [
+        { label: "Saya tidak pernah menunda pekerjaan dan selesai tepat waktu", score: 5 },
+        { label: "Saya suka menunda pekerjaan tetapi selesai tepat waktu", score: 3 },
+        { label: "Saya suka menunda pekerjaan dan terkadang tidak selesai tepat waktu", score: 2 },
+        { label: "Saya suka menunda pekerjaan dan tidak selesai tepat waktu", score: 1 },
+      ]
     },
     {
-      content: "Ceritakan kegagalan Anda.",
+      content: "Bagaimana Anda menilai diri sendiri dibandingkan dengan teman-teman Anda dalam bidang yang Anda lamar saat ini?",
       type: QuestionType.SOFTSKILL,
-      difficulty: "HARD",
-      category: "Personality",
+      difficulty: "MEDIUM",
+      category: "Self Confidence",
+      categoryAnswer: [
+        { label: "Saya sangat unggul dibandingkan teman-teman saya", score: 5 },
+        { label: "Saya unggul dibandingkan teman-teman saya", score: 4 },
+        { label: "Saya setara dengan teman-teman saya", score: 3 },
+        { label: "Saya sedikit lebih buruk dibandingkan teman-teman saya", score: 2 },
+        { label: "Saya jauh lebih buruk dibandingkan teman-teman saya", score: 1 },
+      ]
+    },
+    {
+      content: "Bagaimana sikap anda ketika melihat orang yang lebih ahli atau menguasai bidang yang anda lamar sekarang?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Self Confidence",
+      categoryAnswer: [
+        { label: "Saya merasa termotivasi untuk belajar lebih banyak dan meningkatkan kemampuan saya", score: 5 },
+        { label: "Saya merasa biasa saja dan tetap fokus pada pengembangan diri saya", score: 3 },
+        { label: "Saya merasa minder dan kurang percaya diri dengan kemampuan saya", score: 2 },
+        { label: "Saya merasa tidak percaya diri dan ragu untuk melamar pekerjaan di bidang ini", score: 1 },
+      ]
+    },
+    {
+      content: "Seberapa yakin anda menguasai materi terkait posisi yang anda lamar?",
+      type: QuestionType.SOFTSKILL,
+      difficulty: "MEDIUM",
+      category: "Self Confidence",
+      categoryAnswer: [
+        { label: "Saya sangat yakin menguasai materi terkait posisi yang saya lamar", score: 5 },
+        { label: "Saya yakin menguasai materi terkait posisi yang saya lamar", score: 4 },
+        { label: "Saya kurang yakin menguasai materi terkait posisi yang saya lamar", score: 3 },
+        { label: "Saya tidak yakin menguasai materi terkait posisi yang saya lamar", score: 2 },
+        { label: "Saya tidak menguasai materi terkait posisi yang saya lamar", score: 1 },
+      ]
     },
 
     // TECHNICAL
